10cbdb9800ef385519641007fa1831b4079d45ab
18wh1a1238 <69301824+18wh1a1238@users.noreply.github.com>
2021-01-17 18:12:58 +0530

    Add files via upload

---
+++ CN&WT Lab/CN Experiments/Experiment5/BroadCast_Tree.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+ #include<stdio.h>
+void adjacent(int k,int n,int a[100][100]) {
+	int i,j;
+	printf("Adjacent node of root node:\n");
+	printf("%d\n",k);
+	for(j=1;j<=n;j++) {
+		if((a[k][j] == 1) || (a[j][k] == 1))
+			printf("%d\t",j);
+	}
+	printf("\n");
+	for(i=1;i<=n;i++) {
+		if((a[k][j] == 0) && (a[i][k] == 0) && (i != k))
+		       printf("%d",i);
+	}
+}	
+int main() {
+	int i,j,root_node;
+	int nodes[100][100],total_nodes;
+	printf("Enter total number of nodes:");
+	scanf("%d",&total_nodes);
+	printf("Enter the adjacent matrix");
+	for(i=1;i<=total_nodes;i++) {
+		for(j=1;j<=total_nodes;j++) {
+			printf("Enter the connecting node %d -> %d: ",i,j);
+			scanf("%d",&nodes[i][j]);
+		}
+	}
+	printf("Enter the root node: ");
+	scanf("%d",&root_node);
+	adjacent(root_node,total_nodes,nodes);
+	return 0;
+}
